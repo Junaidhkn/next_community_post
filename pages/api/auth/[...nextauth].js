5cd91982198173a02547a69c39794c7129d7e1cd
Junaidhkn <junaidhkniazi@gmail.com>
2023-02-24 10:11:15 +0500

    prisma adaptor

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,3 +1,14 @@
 import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google";
+import GoogleProvider from "next-auth/providers/google"
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import client from "../../../prisma/client.ts"
 
+export default NextAuth( {
+   adapter: PrismaAdapter( client ),
+   providers: [
+      GoogleProvider( {
+         clientId: process.env.GOOGLE_CLIENT_ID,
+         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      } ),
+   ],
+} )
--- prisma/client.js
+++
@@ -1,5 +0,0 @@
-import { PrismaClient } from '@prisma/client'
-
-const client = globalThis.prisma || new PrismaClient()
-
-export default client
