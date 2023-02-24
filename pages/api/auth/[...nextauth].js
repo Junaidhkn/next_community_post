313c20a92bd5598de46e31a2b56ec20b4195c9a9
Junaidhkn <junaidhkniazi@gmail.com>
2023-02-24 21:00:19 +0500

    refactor [...nextAuth]

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,12 +3,18 @@ import GoogleProvider from "next-auth/providers/google"
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import client from "../../../prisma/client.ts"
 
-export default NextAuth( {
+
+export const authOptions = {
    adapter: PrismaAdapter( client ),
    providers: [
       GoogleProvider( {
          clientId: process.env.GOOGLE_CLIENT_ID,
          clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       } ),
-   ],
-} )
+      // ...add more providers here
+   ]
+}
+
+
+
+export default NextAuth( authOptions )
