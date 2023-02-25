2f1d42721fa2dfe9f8a374f87d9814d9f032b4ff
Junaidhkn <junaidhkniazi@gmail.com>
2023-02-25 22:10:26 +0500

    logged

--- app/auth/Logged.tsx
+++ app/auth/Logged.tsx
@@ -1,7 +1,31 @@
 'use client';
 
-const Logged = () => {
-	return <div>Logged</div>;
+import Image from 'next/image';
+import { signOut } from 'next-auth/react';
+import Link from 'next/link';
+
+type User = {
+	image: string;
 };
 
-export default Logged;
+export default function Logged({ image }: User) {
+	return (
+		<li className='flex gap-8 items-center'>
+			<button
+				className='bg-gray-700 text-white text-sm px-6 py-2 rounded-md '
+				onClick={() => signOut()}>
+				Sign Out
+			</button>
+			<Link href={'/dashboard'}>
+				<Image
+					width={64}
+					height={64}
+					className='w-14 rounded-full'
+					src={image}
+					alt=''
+					priority
+				/>
+			</Link>
+		</li>
+	);
+}
